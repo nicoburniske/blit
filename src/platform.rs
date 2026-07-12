9fab7fa15873ea1bbd604a2ce2866224e0e9a134
Nico Burniske <nicoburniske@gmail.com>
2026-07-12 11:34:36 -0400

    clean up platform organization

--- src/platform.rs
+++ src/platform.rs
@@ -34,65 +34,7 @@ pub struct Platform {
     not_send_or_sync: PhantomData<Rc<()>>,
 }
 
-pub struct PlatformVTable {
-    begin_frame: unsafe fn(NonNull<()>),
-    end_frame: unsafe fn(NonNull<()>),
-    screen: unsafe fn(NonNull<()>) -> PhysicalRect,
-    scale_factor: unsafe fn(NonNull<()>) -> f32,
-    draw_rectangle: unsafe fn(NonNull<()>, &Rectangle, &[PhysicalRect]),
-    create_image: unsafe fn(NonNull<()>, ImageData) -> ImageId,
-    drop_image: unsafe fn(NonNull<()>, ImageId),
-    draw_image: unsafe fn(NonNull<()>, &ImageRequest, &[PhysicalRect]),
-    draw_text: unsafe fn(NonNull<()>, &TextRequest<'_>, &[PhysicalRect]),
-    text_offset_at_position: unsafe fn(NonNull<()>, &TextRequest<'_>, LogicalPoint) -> usize,
-    text_cursor_rect: unsafe fn(NonNull<()>, &TextRequest<'_>, usize) -> LogicalRect,
-    show_keyboard: unsafe fn(NonNull<()>, &KeyboardRequest<'_>),
-}
-
-fn vtable<T: PlatformImpl>() -> &'static PlatformVTable {
-    &PlatformVTable {
-        begin_frame: |data| unsafe { data.cast::<T>().as_mut() }.begin_frame(),
-        end_frame: |data| unsafe { data.cast::<T>().as_mut() }.end_frame(),
-        screen: |data| unsafe { data.cast::<T>().as_mut() }.screen(),
-        scale_factor: |data| unsafe { data.cast::<T>().as_mut() }.scale_factor(),
-        draw_rectangle: |data, request, clips| {
-            unsafe { data.cast::<T>().as_mut() }.draw_rectangle(request, clips)
-        },
-        create_image: |data, image| unsafe { data.cast::<T>().as_mut() }.create_image(image),
-        drop_image: |data, image| unsafe { data.cast::<T>().as_mut() }.drop_image(image),
-        draw_image: |data, image, clips| {
-            unsafe { data.cast::<T>().as_mut() }.draw_image(image, clips)
-        },
-        draw_text: |data, request, clips| {
-            unsafe { data.cast::<T>().as_mut() }.draw_text(request, clips)
-        },
-        text_offset_at_position: |data, request, position| {
-            unsafe { data.cast::<T>().as_mut() }.text_offset_at_position(request, position)
-        },
-        text_cursor_rect: |data, request, byte_offset| {
-            unsafe { data.cast::<T>().as_mut() }.text_cursor_rect(request, byte_offset)
-        },
-        show_keyboard: |data, request| unsafe { data.cast::<T>().as_mut() }.show_keyboard(request),
-    }
-}
-
 impl Platform {
-    pub unsafe fn new<T: PlatformImpl>(implementation: &mut T) -> Self {
-        Self {
-            data: NonNull::from(implementation).cast(),
-            vtable: vtable::<T>(),
-            not_send_or_sync: PhantomData,
-        }
-    }
-
-    pub(crate) fn begin_frame(&mut self) {
-        unsafe { (self.vtable.begin_frame)(self.data) }
-    }
-
-    pub(crate) fn end_frame(&mut self) {
-        unsafe { (self.vtable.end_frame)(self.data) }
-    }
-
     #[inline]
     pub fn screen(&mut self) -> PhysicalRect {
         unsafe { (self.vtable.screen)(self.data) }
@@ -146,5 +88,65 @@ impl Platform {
     #[inline]
     pub fn show_keyboard(&mut self, request: &KeyboardRequest<'_>) {
         unsafe { (self.vtable.show_keyboard)(self.data, request) }
+    }
+}
+
+impl Platform {
+    pub unsafe fn new<T: PlatformImpl>(implementation: &mut T) -> Self {
+        Self {
+            data: NonNull::from(implementation).cast(),
+            vtable: vtable::<T>(),
+            not_send_or_sync: PhantomData,
+        }
+    }
+
+    pub(crate) fn begin_frame(&mut self) {
+        unsafe { (self.vtable.begin_frame)(self.data) }
+    }
+
+    pub(crate) fn end_frame(&mut self) {
+        unsafe { (self.vtable.end_frame)(self.data) }
+    }
+}
+
+pub struct PlatformVTable {
+    begin_frame: unsafe fn(NonNull<()>),
+    end_frame: unsafe fn(NonNull<()>),
+    screen: unsafe fn(NonNull<()>) -> PhysicalRect,
+    scale_factor: unsafe fn(NonNull<()>) -> f32,
+    draw_rectangle: unsafe fn(NonNull<()>, &Rectangle, &[PhysicalRect]),
+    create_image: unsafe fn(NonNull<()>, ImageData) -> ImageId,
+    drop_image: unsafe fn(NonNull<()>, ImageId),
+    draw_image: unsafe fn(NonNull<()>, &ImageRequest, &[PhysicalRect]),
+    draw_text: unsafe fn(NonNull<()>, &TextRequest<'_>, &[PhysicalRect]),
+    text_offset_at_position: unsafe fn(NonNull<()>, &TextRequest<'_>, LogicalPoint) -> usize,
+    text_cursor_rect: unsafe fn(NonNull<()>, &TextRequest<'_>, usize) -> LogicalRect,
+    show_keyboard: unsafe fn(NonNull<()>, &KeyboardRequest<'_>),
+}
+
+fn vtable<T: PlatformImpl>() -> &'static PlatformVTable {
+    &PlatformVTable {
+        begin_frame: |data| unsafe { data.cast::<T>().as_mut() }.begin_frame(),
+        end_frame: |data| unsafe { data.cast::<T>().as_mut() }.end_frame(),
+        screen: |data| unsafe { data.cast::<T>().as_mut() }.screen(),
+        scale_factor: |data| unsafe { data.cast::<T>().as_mut() }.scale_factor(),
+        draw_rectangle: |data, request, clips| {
+            unsafe { data.cast::<T>().as_mut() }.draw_rectangle(request, clips)
+        },
+        create_image: |data, image| unsafe { data.cast::<T>().as_mut() }.create_image(image),
+        drop_image: |data, image| unsafe { data.cast::<T>().as_mut() }.drop_image(image),
+        draw_image: |data, image, clips| {
+            unsafe { data.cast::<T>().as_mut() }.draw_image(image, clips)
+        },
+        draw_text: |data, request, clips| {
+            unsafe { data.cast::<T>().as_mut() }.draw_text(request, clips)
+        },
+        text_offset_at_position: |data, request, position| {
+            unsafe { data.cast::<T>().as_mut() }.text_offset_at_position(request, position)
+        },
+        text_cursor_rect: |data, request, byte_offset| {
+            unsafe { data.cast::<T>().as_mut() }.text_cursor_rect(request, byte_offset)
+        },
+        show_keyboard: |data, request| unsafe { data.cast::<T>().as_mut() }.show_keyboard(request),
     }
 }
