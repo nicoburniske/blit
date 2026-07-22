28ea06652c8fb266f0d9e6fce36f0199b97f2e49
Nico Burniske <nicoburniske@gmail.com>
2026-07-21 23:38:41 -0400

    update flake to use nightly rust

--- flake.nix
+++ flake.nix
@@ -1,13 +1,26 @@
 {
+  inputs.fenix = {
+    url = "github:nix-community/fenix";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { nixpkgs, ... }:
+  outputs = { fenix, nixpkgs, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
       devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [ cargo rust-analyzer rustc rustfmt ];
+        packages = [
+          (fenix.packages.${system}.complete.withComponents [
+            "cargo"
+            "clippy"
+            "rust-src"
+            "rustc"
+            "rustfmt"
+          ])
+          fenix.packages.${system}.rust-analyzer
+        ];
         LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
           libxkbcommon
           wayland
