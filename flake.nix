0cfe7a81b97660d6ff51d8deeb6b45eff385fcfa
Nico Burniske <nicoburniske@gmail.com>
2026-08-23 18:12:09 -0400

    update flake

--- flake.nix
+++ flake.nix
@@ -7,30 +7,37 @@
 
   outputs = { fenix, nixpkgs, ... }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      systems = [
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
     in {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = [
-          (fenix.packages.${system}.complete.withComponents [
-            "cargo"
-            "clippy"
-            "rust-src"
-            "rustc"
-            "rustfmt"
-          ])
-          pkgs.mononoki
-          fenix.packages.${system}.rust-analyzer
-        ];
-        BLIT_TEST_FONT = "${pkgs.mononoki}/share/fonts/opentype/mononoki-Regular.otf";
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
-          libxkbcommon
-          wayland
-          libx11
-          libxcursor
-          libxi
-          libxrandr
-        ]);
-      };
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = pkgs.mkShell {
+            packages = [
+              (fenix.packages.${system}.complete.withComponents [
+                "cargo"
+                "clippy"
+                "rust-src"
+                "rustc"
+                "rustfmt"
+              ])
+              pkgs.mononoki
+              fenix.packages.${system}.rust-analyzer
+            ];
+            BLIT_TEST_FONT = "${pkgs.mononoki}/share/fonts/opentype/mononoki-Regular.otf";
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
+              libxkbcommon
+              wayland
+              libx11
+              libxcursor
+              libxi
+              libxrandr
+            ]);
+          };
+        });
     };
 }
