f96d123dc8c5b9c735c195ab3fd736c4d9611f6e
Nico Burniske <nicoburniske@gmail.com>
2026-08-30 01:00:39 -0400

    remove cx structs

--- kernel/src/leaf.rs
+++ kernel/src/leaf.rs
@@ -1,12 +1,10 @@
-pub use crate::frame::{MeasureCx, PaintCx};
-
 use crate::{
     geometry::{Constraints, Rect, Size},
     renderer::Renderer,
 };
 
 pub trait Leaf<R: Renderer>: Copy + 'static {
-    fn measure(&self, cx: MeasureCx<'_, R>, constraints: Constraints) -> Size;
+    fn measure(&self, renderer: &mut R, constraints: Constraints) -> Size;
 
-    fn paint(&self, cx: PaintCx<'_, R>, area: Rect);
+    fn paint(&self, renderer: &mut R, area: Rect);
 }
